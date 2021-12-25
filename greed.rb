bb92ece3f46c8bc90593a65a1b9bdd820b29edff
apurvaspatil <apurvapatil8888@gmail.com>
2021-12-25 16:40:11 +0530

    Added functionality for non_scoring_dices

--- greed.rb
+++ greed.rb
@@ -1,15 +1,23 @@
 require 'pry'
 def calculate_score(dice)
   triple_scores = [1000, 200, 300, 400, 500, 600]
-    single_scores = [100, 0, 0, 0, 50, 0]
-    score=0
-    (1..6).each do |number|
-      count = dice.count(number)
-      score += triple_scores[number - 1] * (count / 3)
-      score += single_scores[number - 1] * (count % 3)
-      #require 'pry';binding.pry
+  single_scores = [100, 0, 0, 0, 50, 0]
+  score=0
+  non_scoring_count = 5
+  (1..6).each do |number|
+    count = dice.count(number)
+    score += triple_scores[number - 1] * (count / 3)
+    if (count / 3) >= 1
+      non_scoring_count -= 3
     end
-    score
+    score += single_scores[number - 1] * (count % 3)
+    if number == 1 || number == 5 && count % 3 != 0
+      non_scoring_count -= count % 3
+    end
+    #binding.pry
+    
+  end
+  return [score , non_scoring_count]
 end   
 # p calculate_score([1,1,1,1,5])
 
@@ -35,7 +43,10 @@ def turn(scores, player)
   roll = dice.values
 
   puts "The roll is: #{roll.inspect}"
-  score = calculate_score(roll)
+
+  temp = calculate_score(roll)
+  score = temp[0]
+  non_scoring_dice = temp[1]
   puts "Score of #{player}: #{score}"
   scores[player] += score
   scores
