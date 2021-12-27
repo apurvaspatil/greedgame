00ca15e517d645039134a715560c59051c0ee011
Apurva Patil <apurvapatil8888@gmail.com>
2021-12-27 20:27:51 +0530

    entering final round

--- greed.rb
+++ greed.rb
@@ -49,6 +49,7 @@ def turn(scores, player)
   current_score = temp[0]
   puts "score #{score}"
   non_scoring_count = temp[1]
+
   while non_scoring_count != 0 && current_score !=0
     puts "Do you want to re-roll? the non scoring dice #{non_scoring_count} (y/n)?"
     choice = gets.chomp
@@ -91,9 +92,11 @@ round = 1
 
 def did_anyone_win(scores)
   scores.each do |score|
-    if score >= 1000
+    if score >= 3000
       return true
+      #final_score = final_round(scores)
     end
+    #final_score
   end
   return false
 end
@@ -110,3 +113,48 @@ while !did_anyone_win(scores)
   round += 1
   puts ""
 end
+
+
+player_who_reach3000= []
+scores.each_index do |index|
+  if scores[index] >= 3000
+    player_who_reach3000.push(index)
+  end
+end
+
+
+
+puts "--------entering the final round--------"
+  
+(0...n).each do |player|
+  if !player_who_reach3000.include?player
+    scores = turn(scores, player)
+  end
+end
+puts "Scores: #{scores}"
+
+playerid_havingMax_score= []
+def max_score(scores)
+  max_number = scores[0]
+  scores.each_index do |index|
+    if scores[index] > max_number
+      max_number = scores[index]
+    end
+  end
+  max_number
+end
+
+scores.each_index do |index|
+  if scores[index] == max_score(scores)
+    playerid_havingMax_score.push(index)
+  end
+end
+
+#puts playerid_havingMax_score
+playerid_havingMax_score.each do |player|
+  puts " the player who won is #{player+1}"
+end
+
+  
+
+
